6ca648193cf1b6930699e3c53aa98d34d9ba2a51
Luca De Vitis <luca.devitis@moneysupermerket.com>
2017-01-08 12:12:37 +0000

    better spec coverage

--- rspec/spec/brownbag/bin/check_transaction_spec.rb
+++ rspec/spec/brownbag/bin/check_transaction_spec.rb
@@ -1,34 +1,33 @@
 # I don't want to be bothered with line lenght here.
 # rubocop:disable Metrics/LineLength
-require 'bin_spec_helper'
+require 'spec_helper_bin'
 require 'brownbag/bin/check_transactions'
 
 describe Brownbag::Bin::CheckTransactions do
+  # `config` is always empty for this spec, but I want to show you that with a
+  # carfull/clevar `argv` and `config` usage we can create 100% test friendly
+  # check/metrics/scripts.
+  #
   # If not specified, default `subject` would be
   # `Brownbag::Bin::CheckTransactions.new(ARGV)` with `ARGV` being rspec
   # command's `ARGV`.
   #
-  # I could create a subject with an argv like the one I would use, but that
-  # would just test `Mixlib::CLI` argument parsing methods. Also, I want to
-  # show you that with a carfuli/clevar `argv` and `config` usage we can create
-  # 100% test friendly check/metrics/scripts.
-  subject { Brownbag::Bin::CheckTransactions.new([]) }
-
   # Given a set of: argv, config, exit status, standard output matching reg exp
   [
-    [['spec/fixtures/files/transactions/ok.log'],         {},               0, /OK/],
-    [['spec/fixtures/files/transactions/warning.log'],    { warning: 30 },  0, /OK/],
-    [['spec/fixtures/files/transactions/warning.log'],    {},               1, /WARNING: 40/],
-    [['spec/fixtures/files/transactions/overdraft.log'],  {},               2, /CRITICAL: Not enough money/],
-    [['spec/fixtures/files/transactions/broken.log'],     {},               3, /UNKNOWN: fail/],
-    [[],                                                  {},               3, /UNKNOWN: transactions log/]
+    [['spec/fixtures/files/transactions/ok.log'],                   {},  0, /OK/],
+    [%w(--warning 30 spec/fixtures/files/transactions/warning.log), {},  0, /OK/],
+    [['spec/fixtures/files/transactions/warning.log'],              {},  1, /WARNING: 40/],
+    [['spec/fixtures/files/transactions/overdraft.log'],            {},  2, /CRITICAL: Not enough money/],
+    [['spec/fixtures/files/transactions/broken.log'],               {},  3, /UNKNOWN: fail/],
+    [[],                                                            {},  3, /UNKNOWN: transactions log/]
   ].each do |argv, config, exit_status, stdout|
     # I could do it with a shared example, however it has a slightly different
     # meaning.
     context "when argv = #{argv}, config = #{config}" do
-      # Override subject's `argv` and `config` attributes
+      subject { Brownbag::Bin::CheckTransactions.new(argv) }
+
+      # Override subject's `config` attribute
       before(:example) do
-        subject.argv = argv
         subject.config.update(config)
       end
       it "should exit with status #{exit_status} and output matching #{stdout.inspect}" do
