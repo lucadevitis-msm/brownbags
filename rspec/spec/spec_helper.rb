876cdf42f94d65716fefd6c5ea8e5aba7649a893
Luca De Vitis <luca.devitis@moneysupermerket.com>
2017-01-08 12:08:02 +0000

    ignore beaker logs

--- rspec/spec/spec_helper.rb
+++ rspec/spec/spec_helper.rb
@@ -1,7 +1,2 @@
-require 'puppetlabs_spec_helper/module_spec_helper'
-
-RSpec.configure do |configure|
-  configure.after(:suite) do
-    RSpec::Puppet::Coverage.report! # (95)
-  end
-end
+require 'simplecov'
+require 'rspec'
