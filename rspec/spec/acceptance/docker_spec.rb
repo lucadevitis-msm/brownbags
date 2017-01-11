3cf1d63562502d9772ed7dd48ad52a10cc76d1a1
Luca De Vitis <luca.devitis@moneysupermarket.com>
2017-01-11 14:46:51 +0000

    Bypass vagrant by using docker

--- rspec/spec/acceptance/docker_spec.rb
+++ rspec/spec/acceptance/docker_spec.rb
@@ -27,22 +27,40 @@ describe host('localhost') do
   # Test remote connections
   describe host('www.google.com') do
     it { is_expected.to be_resolvable.by('dns') }
-    it { is_expected.to be_reachable }
+    it { is_expected.to be_reachable.with(port: 80, proto: 'tcp', timeout: 5) }
   end
 
-  # Supports multiple package managers
-  # describe package('ruby') do
-  #   it { is_expected.to be_installed.by(:rpm).with_version('1.8.7') }
-  # end
+  # This is a regression test I inserted after I found out that `be_reachable`
+  # implementation in Specinfra tend to use `nc` command most of the times, but
+  # `nc` is not installed by default.`
+  # Supports multiple package managers.
+  describe package('nc') do
+    it { is_expected.to be_installed.by(:rpm).with_version('1.84') }
+  end
 
   # Test the outcome of specific commands
   describe command('netstat') do
     its(:exit_status) { is_expected.to eq 0 }
   end
 
+  # You can inspect running processes in great detail. For the purpose of this
+  # brownbag we'll take advantage of the sshd that Beaker will spin up.
+  # Remember, Serverspec/Specinfra can run commands directly into the container
+  # using a Docker command dispatcher. However, `beaker-rspec` defines and uses
+  # a Beaker specific command dispatcher that sits on top of the
+  # Serverspec/Specinfra ssh one. For semplicity, Beaker spins up sshd on any
+  # kind of instance so it can use the same approach regardles of the instance
+  # type.
+  sshd_pidfile = '/var/run/sshd.pid'
+  describe file(sshd_pidfile) do
+    it { is_expected.to exist }
+    it { is_expected.to be_file }
+    its(:size) { is_expected.to be > 0 }
+    its(:content) { is_expected.to match(/^[0-9]+$/) }
+  end
   # Test any attribute that you can get from `ps` command
-  describe process('init') do
-    its(:pid) { is_expected.to eq 1 }
+  describe process('sshd') do
+    its(:pid) { is_expected.to eq command("cat #{sshd_pidfile}").stdout.to_i }
     its(:user) { is_expected.to eq 'root' }
   end
 
