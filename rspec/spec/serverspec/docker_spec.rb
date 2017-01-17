5cf56e1ed474ef6a2f76d6bc33d660423ab0e8e5
Luca De Vitis <luca.devitis@moneysupermarket.com>
2017-01-17 15:05:59 +0000

    serverspec to inspect docker container/image

---
+++ rspec/spec/serverspec/docker_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper_serverspec'
+
+# RSpec interface to `docker inspect`
+describe docker_image('centos:6.6') do
+  it { is_expected.to exist }
+
+  # Use `its(:inspection)` to access `subject.inspection` which is the output
+  # of `docker inspect`
+  its(:inspection) do
+    is_expected.to include('Architecture' => 'amd64', 'Os' => 'linux')
+  end
+end
+
+# We can also inspect a running container
+describe docker_container('brownbag') do
+  it { is_expected.to exist }
+  it { is_expected.to be_running }
+  it { is_expected.not_to have_volume('/tmp', '/data') }
+
+  # Use lists as argument for `its` to make use the `[]` operator of `subject`
+  # in order to access `inspection` data.
+  its(['HostConfig.NetworkMode']) { is_expected.to eq 'bridge' }
+  its(['Config.Image']) { is_expected.to eq 'centos:6.6' }
+end
