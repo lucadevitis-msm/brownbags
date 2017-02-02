207e515dafddef68d9e2754906ddc3de906e87b0
Luca De Vitis <luca.devitis@moneysupermarket.com>
2017-02-02 10:48:08 +0000

    removed ambiguous spec

--- rspec/spec/functions/to_conf_spec.rb
+++ rspec/spec/functions/to_conf_spec.rb
@@ -2,7 +2,6 @@
 require 'spec_helper'
 
 describe 'to_conf' do
-  it { is_expected.not_to eq(nil) }
   it { is_expected.to run.with_params.and_raise_error(ArgumentError) }
   it { is_expected.to run.with_params('key' => 'value').and_return(/key: value/) }
 end
